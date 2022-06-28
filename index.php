2d01fed993a56e18e2b5de9e0ded3c422ad7d781
Mohammed-Alijl <moh.wa.2016@gmail.com>
2022-06-28 17:57:37 +0300

    some update

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-require_once("header.php");
+require_once("header.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
 if (isset($_SESSION['id'])) {
 ?>
     <section>
@@ -15,7 +31,7 @@ if (isset($_SESSION['id'])) {
 <?php } else { ?>
     <section>
         <div class="container">
-        <h2>you are not log in you can:</h2>
+            <h2>you are not log in you can:</h2>
             <ul>
                 <li><a href="signup.php">sign up</a></li>
                 <li><a href="login.php">login to your account</a></li>
--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if (isset($_GET['log_in_error'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/master.css">
-    <title>sign up</title>
+    <title>log in</title>
 </head>
 
 <body>
